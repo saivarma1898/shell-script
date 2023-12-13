870304e1f7917cf7257e3df58ff4ec9a5e26340e
sai <varma.kalidhindi999@gmail.com>
2023-12-13 22:56:08 +0530

    c

--- mqsql.sh
+++ mqsql.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-
+set -e
+set -x
 ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
 echo "ERROR:: Please run this script with root access"
- exit 1 # you can give other than 0
+# exit 1 # you can give other than 0
 else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-yum install mysql -y
+yum install mysl -y
 
 if [ $? -ne 0 ]
 then
     echo "ERROR:: Installing MySQL is failed"
-    exit 1
+ #   exit 1
 else
     echo "Installing MySQL is SUCCESS"
 fi
